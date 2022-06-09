fc8e4fbdbd8b08230cfe871278e3d2102be2b491
Cuong Nguyen <3936946+cuongptnk@users.noreply.github.com>
2022-06-09 10:26:47 -0300

    init

---
+++ RickAndMorty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Wattpad Cuong on 2022-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Wattpad Cuong on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
